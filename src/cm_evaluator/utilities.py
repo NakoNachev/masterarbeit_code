84e70f3f337c7e572f784a17e1f62eed03f6bef8
NakoNachev <nako.n.nachev@gmail.com>
2025-01-16 21:34:49 +0100

    remove old get_remaining_master_sentences

--- src/cm_evaluator/utilities.py
+++ src/cm_evaluator/utilities.py
@@ -155,29 +155,6 @@ def common_words(words1: List[str], words2: List[str], model: SentenceTransforme
             similar_concepts.append(word)
     return similar_concepts
 
-# def get_remaining_master_sentences(
-#         master_sentences: List[str], 
-#         student_sentences: List[str], 
-#         model: SentenceTransformer, 
-#         threshold: float = 0.8):
-#     """ Returns the sentences from the master solution that were not correctly identified
-#     Correctly identified sentences = sentence which has either 1.0 or >= threshold similarity
-#     with at least one of the master solution sentences"""
-#     correct_sentences = []
-
-#     if len(student_sentences) == 0:
-#         return set(master_sentences)
-
-#     matrix = get_nearest_sentence_matrix(master_sentences, student_sentences, model)
-#     print("matrix", matrix)
-    
-#     for i, sentence in enumerate(master_sentences):
-#         best_match_i = np.argmax(matrix[i])
-#         similarity = round(matrix[i][best_match_i],2)
-#         if similarity == 1 or similarity >= threshold:
-#             correct_sentences.append(sentence)
-#     return set(master_sentences) - set(correct_sentences)
-
 
 def get_remaining_master_sentences(
         master_sentences: List[str], 
